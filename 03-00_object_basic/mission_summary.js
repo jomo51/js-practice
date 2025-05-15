5e01b53d66a9e832df20249f6e4176d93dceac5e
KD.Nakagawa <nakagawa@kdb.co.jp>
2025-05-15 13:27:50 +0900

    ADD:課題

---
+++ 03-00_object_basic/mission_summary.js
@@ -0,0 +1,42 @@
+const employees = {
+  e001: { name: "Yamada", age: 30, gender: "male" },
+  e002: { name: "Tanaka", age: 25, gender: "female" },
+  e003: { name: "Suzuki", age: 32, gender: "male" },
+  e004: { name: "Sato", age: 29, gender: "female" },
+  e005: { name: "Kato", age: 28, gender: "male" }
+};
+
+//データのみ抽出
+
+const employeesSummary = GetEmployeeSummary(Object.values(employees));
+
+Object.entries(employeesSummary).forEach(([key,value]) => {
+  console.log(`🔻${key}`);
+  console.log(`人数: ${value.peopleCnt}`);
+  console.log(`平均年齢: ${value.averageAge}`);
+  console.log(`名前一覧: ${value.peopleNames}`);
+  console.log('');
+});
+
+//サマリーを出す関数
+function GetEmployeeSummary(employeesData) {
+  const males = employeesData.filter(employees => employees.gender === 'male');
+  const females = employeesData.filter(employees => employees.gender === 'female');
+
+  return ({
+    males: getSummary(males),
+    females: getSummary(females)
+  })
+}
+
+//集計する関数
+function getSummary(genderDatas){
+  return ({
+    peopleCnt :
+      genderDatas.reduce((sum , genderData) => sum + 1,0),
+    averageAge :
+      genderDatas.reduce((sum, genderData) => sum + genderData.age,0) / genderDatas.length,
+    peopleNames :
+      genderDatas.map(genderData => genderData.name)
+  });
+}
