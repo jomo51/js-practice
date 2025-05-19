66817bac0b7b01dbfccdb29e74fc5229633f8881
KD.Nakagawa <nakagawa@kdb.co.jp>
2025-05-19 17:53:00 +0900

    FIX:typo修正

--- 03-03_class_and_prototype/basic_class.js
+++ 03-03_class_and_prototype/basic_class.js
@@ -1,10 +1,13 @@
-class user {
+class User {
   constructor(name,age) {
     this.name = name;
     this.age = age;
   }
 
-  great() {
+  greet() {
     console.log(`Hi, I'm ${this.name} (${this.age})`);
   }
 }
+
+const jomo = new User('jomo',37);
+jomo.greet();
